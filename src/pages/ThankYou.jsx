0905f689b93048b5483bb45b6ec1a4252b6a8332
mukesh <sutharmukesh910@gmail.com>
2024-04-17 12:54:46 +0530

    new commit

--- src/pages/ThankYou.jsx
+++ src/pages/ThankYou.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Container, Row, Col, Button } from 'reactstrap'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 import '../styles/thank-you.css'
 
 const ThankYou = () => {
